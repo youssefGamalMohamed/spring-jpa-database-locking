6867e53572af7df6b308c12de7884fa77fcd10f3
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-03-05 00:30:58 +0200

    chore: Initialize App with Dependcies and Required Configs

---
+++ src/main/java/com/youssef_gamal/database_locking/DatabaseLockingApplication.java
@@ -0,0 +1,13 @@
+package com.youssef_gamal.database_locking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseLockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseLockingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youssef_gamal/database_locking/DatabaseLockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef_gamal.database_locking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseLockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
